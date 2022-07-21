23db92bfa319bc32a3c389e546d2b6486770941e
mmarkk <mmarkk@xtra.co.nz>
2022-07-21 23:45:15 +1200

    Initial Commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,37 @@
+<script>
+	let color = '#ff3e00';
+	$: code = '';
+</script>
+
+<div style="--theme-color: {color}">
+	<div class="mt-8">
+		<div>
+			<label for="comment" class="block text-gray-700 text-xl font-bold">Paste svg code here</label>
+			<div class="mt-1">
+				<textarea
+					rows="10"
+					bind:value={code}
+					name="comment"
+					id="comment"
+					class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+				/>
+			</div>
+		</div>
+	</div>
+
+	<label for="iconcolor" class="block text-xl font-bold text-gray-700 mt-8">Select color here</label
+	>
+	<input type="color" bind:value={color} style="height: 50px;" />
+
+	<div style="--theme-color: {color}">
+		<p class="icon mx-auto mt-8">{@html code}</p>
+	</div>
+</div>
+
+<style>
+	.icon {
+		fill: var(--theme-color);
+		height: 80px;
+		width: 80px;
+	}
+</style>
